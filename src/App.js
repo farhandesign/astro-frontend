e29adfd763fe4d0ef36a0527165ab18f868e1041
abdell-99 <71258550+abdell-99@users.noreply.github.com>
2021-03-22 17:53:39 +0400

    react route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import NavBar from './Components/NavBar/NavBar';
 import Home from './Pages/Home/Home';
+import SignupPage from './Pages/Auth/SignupPage';
+
 
 function App() {
 	return (
 		<div className="App">
-			<NavBar links={[ 'Events', 'Create An Event' ]} />
-			<Home />
+			<BrowserRouter>
+				<Switch>
+				<LayoutRoute path="/" exact={true} component={Home} />
+				<LayoutRoute path="/signup" exact={true} component={SignupPage} />
+				</Switch>
+			</BrowserRouter>
 		</div>
 	);
 }
---
+++ src/Pages/LayoutRoute.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NavBar from '../Components/NavBar/NavBar';
+
+const LayoutRoute = (props) => {
+    return(
+        <div>
+            <NavBar links={[ 'Events', 'Create An Event' ]} />
+            <div className="content" style={{ minHeight: 'calc(100vh - 112px)' }}>
+				<Route path={props.path} exact={props.exact} component={props.component} />
+			</div>
+        </div>
+    )
+
+}
+export default LayoutRoute;
