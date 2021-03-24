040f269a0599b863757cd979856ad5d933e1d7b2
farhandesign <farhan.design@outlook.com>
2021-03-24 10:30:01 +0400

    added a single event route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './Pages/Home/Home';
 import SignupPage from './Pages/Auth/SignupPage';
 import LayoutRoute from './Pages/LayoutRoute';
+import Event from './Pages/Events/Event';
 import CreateEvent from './Pages/Events/CreateEvent';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
 					<LayoutRoute path="/" exact={true} component={Home} />
 					<LayoutRoute path="/signup" exact={true} component={SignupPage} />
 					<LayoutRoute path="/create-event" exact={true} component={CreateEvent} />
+					<LayoutRoute path="/event" exact={true} component={Event} />
 				</Switch>
 			</BrowserRouter>
 		</div>
