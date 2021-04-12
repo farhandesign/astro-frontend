81a90821c9f3d548d3256ec386db2daaa1638043
farhandesign <farhan.design@outlook.com>
2021-04-12 07:31:04 +0400

    no change

--- src/Pages/LayoutRoute.js
+++ src/Pages/LayoutRoute.js
@@ -3,14 +3,13 @@ import { Route } from 'react-router-dom';
 import NavBar from '../Components/NavBar/NavBar';
 
 const LayoutRoute = (props) => {
-    return(
-        <div>
-            <NavBar links={[ 'Events', 'Create An Event' ]} />
-            <div className="content" style={{ minHeight: 'calc(100vh - 112px)' }}>
+	return (
+		<div>
+			<NavBar links={[ 'Events', 'Create An Event' ]} />
+			<div className="content" style={{ minHeight: 'calc(100vh - 112px)' }}>
 				<Route path={props.path} exact={props.exact} component={props.component} />
 			</div>
-        </div>
-    )
-
-}
+		</div>
+	);
+};
 export default LayoutRoute;
